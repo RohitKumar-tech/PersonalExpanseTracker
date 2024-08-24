2c7c5a70562b7ae5c614bcae590d81b8396fb324
rohitkumar <rohitkumar620200@gmail.com>
2024-08-24 21:41:05 +0530

    First commit

---
+++ src/main/java/com/java/personalexpansetracker/PersonalExpanseTracker.java
@@ -0,0 +1,8 @@
+package com.java.personalexpansetracker;
+
+public class PersonalExpanseTracker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
